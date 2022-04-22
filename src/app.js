41c8df7e7c4d4a786e4546a7031492cdaa28dc50
Jennifer Tang <jt3744@nyu.edu>
2022-04-22 02:46:25 -0400

    filter working

--- src/app.js
+++ src/app.js
@@ -14,19 +14,50 @@ const publicPath = path.resolve(__dirname, "public");
 app.use(express.static(publicPath));
 
 // body parser
-app.use(express.urlencoded({ extended: false }));
-app.set('view engine', 'hbs');
+/* app.use(express.urlencoded({ extended: false }));
+app.set('view engine', 'hbs'); */
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 const Review = mongoose.model('Review');
 
-app.get('/api/reviews', function(req, res) {
+app.get('/api/reviews', async(req, res) => {
   // TODO: retrieve all reviews or use filters coming in from req.query
   // send back as JSON list
+  const sem = req.query.semester;
+  const yr = req.query.year;
+  const check = {
+    ...(sem && {semester: sem}),
+    ...(yr && {year: yr}),
+ };
+ //console.log(check);
+  const reviews = await Review.find(check).exec();
+  //console.log(reviews);
+  const data = reviews.map(r => {
+    return {name: r.name, semester: r.semester, year: r.year, review: r.review};
+  });
+  res.json(data);
 });
 
 app.post('/api/review/create', (req, res) => {
   // TODO: create new review... if save succeeds, send back JSON
   // representation of saved object
+  req.setRequestHeader("Content-type", "");
+  const review = new Review(req.body);
+  
+  review.save(function(err, review) {
+    if (err) {
+      console.log(err);
+      res.status(500).send(err);
+    } else {
+      res.json(review);
+    }
+  });
+  //console.log(Reviews.find());
+/*   const {text, from} = req.body;
+  const saved = await (new Message({text, from})).save();
+  res.send(saved); */
 });
 
 app.listen(process.env.PORT || DEFAULT_AIT_PORT, (err) => {
--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,6 +1,113 @@
 // TODO: add client side code for single page application
+document.addEventListener("DOMContentLoaded", main);
 function main() {
-
+    const btn = document.querySelector('#filterBtn');
+    btn.addEventListener('click', handleClick);
+    const btn2 = document.querySelector('#addBtn');
+    btn2.addEventListener('click', handleClick2);
+    loadMessages();
 }
 
-document.addEventListener("DOMContentLoaded", main);
+async function handleClick(evt) {
+    evt.preventDefault();
+    const sem = document.querySelector('#filterSemester').value;
+    const yr = document.querySelector('#filterYear').value;
+    const config = {
+      method: 'GET',
+      headers: {
+        "Content-Type": 'application/json'
+      }
+    };
+    let url = "";
+    if(sem && yr){
+        url = `http://localhost:3000/api/reviews?semester=${sem}&year=${yr}`;
+    }
+    else if(sem){
+        url = `http://localhost:3000/api/reviews?semester=${sem}`;
+    }
+    else if(yr){
+        url = `http://localhost:3000/api/reviews?year=${yr}`;
+    }
+    else{
+        url = "http://localhost:3000/api/reviews";
+    }
+    const res = await fetch(url, config);
+    const reviews = await res.json();
+    //addMessagesToPage([review]);
+    let element = document.querySelector('tbody');
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    }   
+    for(const m of reviews){
+        const tr = document.createElement('tr');
+        let td1 = tr.appendChild(document.createElement('td'));
+        td1.innerText = m.name;
+        let td2 = tr.appendChild(document.createElement('td'));
+        td2.innerText = m.semester;
+        let td3 = tr.appendChild(document.createElement('td'));
+        td3.textContent = m.year;
+        let td4 = tr.appendChild(document.createElement('td'));
+        td4.textContent = m.review;
+        document.querySelector('tbody').appendChild(tr);
+    }
+    console.log(element);
+  }
+
+  async function handleClick2(evt) {
+    const sem = document.querySelector('#semester').value;
+    const yr = document.querySelector('#year').value;
+    const name = document.querySelector('#name').value;
+    const review = document.querySelector('#review').value;
+
+    const config = {
+      method: 'POST',
+      headers: {
+        "Content-Type": 'application/json'
+      },
+      body: JSON.stringify({name: name, semester: sem, year: yr, review: review})
+    };
+
+    const res = await fetch("http://localhost:3000/api/reviews/create", config);
+    const reviews = await res.json();
+    //addMessagesToPage([msg]);
+    console.log(res);
+/*     let element = document.querySelector('tbody');
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    }   
+    for(const m of reviews){
+        const tr = document.createElement('tr');
+        let td1 = tr.appendChild(document.createElement('td'));
+        td1.innerText = m.name;
+        let td2 = tr.appendChild(document.createElement('td'));
+        td2.innerText = m.semester;
+        let td3 = tr.appendChild(document.createElement('td'));
+        td3.textContent = m.year;
+        let td4 = tr.appendChild(document.createElement('td'));
+        td4.textContent = m.review;
+        document.querySelector('tbody').appendChild(tr);
+    } */
+  }
+
+async function loadMessages() {
+    const res = await fetch('http://localhost:3000/api/reviews')
+    const reviews = await res.json();
+    //const rows = document.querySelector('tbody');
+    //divs.forEach(d => d.remove());
+    console.log(reviews);
+    for(const m of reviews) {
+        const tr = document.createElement('tr');
+        let td1 = tr.appendChild(document.createElement('td'));
+        td1.innerText = m.name;
+        let td2 = tr.appendChild(document.createElement('td'));
+        td2.innerText = m.semester;
+        let td3 = tr.appendChild(document.createElement('td'));
+        td3.textContent = m.year;
+        let td4 = tr.appendChild(document.createElement('td'));
+        td4.textContent = m.review;
+        document.querySelector('tbody').appendChild(tr);
+        
+        //div.textContent = m.from + '...  ' + m.text;
+      } 
+    //setTimeout(loadMessages, 500);
+    }
